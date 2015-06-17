6ed8c1ef6af169d51783b75c6c3ba894143cc58b
Hamid Musavi <musavi_hamid@bah.com>
2015-06-17 09:10:58 -0400

    ODE-79 Integrate Clearinghouse subscription into a Spark workflow

--- ode/Development/apps/ode-commons/src/main/java/com/bah/ode/wrapper/DataProcessor.java
+++ ode/Development/apps/ode-commons/src/main/java/com/bah/ode/wrapper/DataProcessor.java
@@ -1,7 +1,19 @@
 package com.bah.ode.wrapper;
 
+import com.bah.ode.model.OdeException;
+
 
 public interface DataProcessor<T> {
-   void process(T data);
+   void process(T data) throws DataProcessorException;
+   
+   public static class DataProcessorException extends OdeException {
+
+      public DataProcessorException(Exception e) {
+         super(e);
+      }
+
+      private static final long serialVersionUID = 1L;
+      
+   }
 
 }
