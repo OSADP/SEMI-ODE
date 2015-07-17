8afb557a51e21225bed43b1b56c438492af571e0
Matthew Kowalsky <kowalsky_matthew@bah.com>
2015-07-17 17:05:25 -0400

    ODE-44 Including Sample Config File that  allows overrides for all parameters. Updated test code to parse sample config. as well as load valdiation file into memory. Validation logic is pending.

--- ode/Development/emulatedClientApps/SituationalDataApp/websocket-test.py
+++ ode/Development/emulatedClientApps/SituationalDataApp/websocket-test.py
@@ -1,150 +1,276 @@
 #!/bin/env python
 
-import websocket
-import thread
-import time
-import json
-import sys
+import thread, time, datetime, json, sys, os
+import logging
 
-from optparse import OptionParser,SUPPRESS_HELP,OptionGroup
+from optparse import OptionParser, SUPPRESS_HELP, OptionGroup
 from collections import defaultdict
+import ConfigParser
 
+import websocket
 import timehelpers
+# Set TIME to UTC
+# logging.Formatter.converter = time.gmtime
 
-base_config={}
+logger = logging.getLogger('simaApp')
+logger.setLevel(logging.DEBUG)
+# fh = logging.FileHandler('spam.log')
+# fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler(stream=sys.stdout)
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s-%(levelname)s: %(message)s', datefmt="%Y-%m-%dT%H:%M:%S")
+# fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+# logger.addHandler(fh)
+logger.addHandler(ch)
+
+base_config = {}
 config = defaultdict(lambda: None, base_config)
-areas_subs = ["veh", "int", "agg" ]
+
+json_file_data = []
+
+message_codes = [
+    'SUCCESS',
+    'FAILURE',
+    'SOURCE_CONNECTION_ERROR',
+    'INVALID_REQUEST_TYPE_ERROR',
+    'INVALID_DATA_TYPE_ERROR'
+]
+
+areas_subs = ["veh", "int", "agg"]
 area = {
     'nwLat': "43.652969118285434",
     'nwLon': "-85.94707489013672",
     'seLat': "36.4765153148293",
     'seLon': "-74.53468322753906",
-    }
+}
 common_params = {
-    "skip":"0",
+    "skip": "0",
     "limit": "10"
-    }
+}
 
+start_date = datetime.datetime(2015, month=1, day=1,hour=1, minute=1,second=30, tzinfo=timehelpers.ZULU()) #2015-01-01T01:01:30.000Z
+end_date = datetime.datetime(2015, month=6, day=1, hour=23, minute=59, second=59, microsecond=999,
+                             tzinfo=timehelpers.ZULU()) # 2015-06-15T23:59:59.999Z
+date_format = '%Y-%m-%dT%H:%M:%S.%f%Z'
 
 qry_subs = \
     {
-    "veh": {
-           "nwLat": "43.652969118285434",
-           "nwLon": "-85.94707489013672",
-           "seLat": "36.47651531482931",
-           "seLon": "-74.53468322753902",
-           "startDate": "2015-01-01T15:16:15.924Z",
-           "endDate": "2015-07-02T15:16:15.924Z",
-           },
-    "adv": {
-           "nwLat": "43.652969118285434",
+        "veh": {
+            "nwLat": "43.652969118285434",
+            "nwLon": "-85.94707489013672",
+            "seLat": "36.47651531482931",
+            "seLon": "-74.53468322753902",
+            "startDate": start_date.strftime(date_format),
+            "endDate": end_date.strftime(date_format)
+        },
+        "adv": {
+            "nwLat": "43.652969118285434",
             "nwLon": "-85.94707489013672",
             "seLat": "36.4765153148293",
             "seLon": "-74.53468322753901",
-            "startDate": "2015-01-01T15:17:33.227Z",
-            "endDate": "2015-07-02T15:17:33.227Z",
+            "startDate": start_date.strftime(date_format),
+            "endDate": end_date.strftime(date_format)
 
-            },
-    "int": {
-        "nwLat": "43.652969118285499",
-        "nwLon": "-85.94707489013672",
-        "seLat": "36.47651531482931",
-        "seLon": "-74.53468322753906",
-        "startDate": "2015-01-01T15:17:33.227Z",
-        "endDate": "2015-07-02T15:17:33.227Z",
+        },
+        "int": {
+            "nwLat": "43.652969118285199",
+            "nwLon": "-85.94707489013672",
+            "seLat": "36.47651531482932",
+            "seLon": "-74.53468322753905",
+             "startDate": start_date.strftime(date_format),
+            "endDate": end_date.strftime(date_format)
 
-     },
+        },
     }
 
-msg = {} # Empty Message body
+msg = {}  # Empty Message body
+
 json_file = None
+
+def append_to_file(entry):
+    with open("ODE_Output.txt", "a") as myfile:
+        myfile.write(entry.encode('UTF-8')+"\n")
+
 # Web Socket Handlers
-
 def on_message(ws, message):
+    """
+    Handler that processes messages received by the web socket
+    """
 
+    # determine message and act accordingly
+    logger.debug(message)
+    #append_to_file(message)
+    try:
+        msg = json.loads(message)
 
-    msg = json.loads(message)
-    #print (json.dumps(msg,indent=1, separators=(',', ': ')))
-    print (repr(message) + "\n")
-    # Remove leading and trailing quotes, replace '\' with empty spaces
+        if msg.get('code'):
+            logger.info("ODE Connection Status: %s Message: %s", msg.get('code'), msg.get('message'))
+        if msg.get('serialId'):
+            validate_message(msg)
+        if msg.get('payload'):
+            logger.info(msg['payload']['dialogID']['mValue'])
+            logger.debug(msg.keys())
+            validate_message(msg)
+        if msg.get("fullMessage"):
+            logger.info(msg.get("fullMessage"))
+            on_close(ws)
+    except:
+        logger.exception("Unable to convert message to json dictionary object")
 
+        ## validate message to that which is stored locaally
+        # iterating over local message  to ensure ordering and values are correct
 
-    # /sub/int
-    print "XXXXXXXXXXXXXXXXXXXXXXXXXXXX\n"
-    print msg.keys()
-    print "XXXXXXXXXXXXXXXXXXXXXXXXXXXX\n"
-
-    ## validate message to that which is stored locaally
-    # iterating over local message  to ensure ordering and values are correct
 
 def on_error(ws, error):
-    ws.close()
-    print (error)
-    print ("### closed on error ###")
+    """
+    Stop the subscription / query on the ODE and then close the socket.
+    :param ws:
+    :param error:
+    :return:
+    """
+    # ws.close()
+    logger.exception(error)
+    logger.critical("Closing Web Socket Connection")
+    on_close(ws)
+
 
 def on_close(ws):
     ws.close()
-    print ("### closed ###" )
+    logger.info('Closing Web Socket')
+    sys.exit()
 
 
 def on_open2(ws):
-    def run(*args):
-        print 'msg payload'
-        print(json.dumps(msg))
-        ws.send(json.dumps(msg))
-        time.sleep(150)
-        ws.close()
-        print ("thread terminating...")
-    thread.start_new_thread(run, ())
-
-def get_json_file():
     """
-    Get correct json file based on subscription type
+     On Web Socket Open Handler
+     Transmits a request to the ODE to start subscription/query
+
+    :param ws:
     :return:
     """
-    pass
+    pause = 30
+
+    def run(*args):
+        logger.debug(json.dumps(msg))
+        time.sleep(pause)
+        ws.send(json.dumps(msg))
+        time.sleep(pause)
+        #ws.close()
+        logger.info ("On_open Run thread stopping...")
+
+    thread.start_new_thread(run, ())
+
+
+def get_json_file(test_type):
+    """
+    Get correct json file based on subscription type input Tuple
+    :return:
+    """
+    os.getcwdu()
+    json_files = {
+        ('sub', 'veh'): os.path.join('.', 'test_json', 'sub_veh.json'),
+        ('sub', 'int'): os.path.join('.', 'test_json', 'sub_int.json'),
+        ('sub', 'agg'): os.path.join('.', 'test_json', 'sub_agg.json'),
+        ('qry', 'veh'): os.path.join('.', 'test_json', 'qry_veh.json'),
+        ('qry', 'int'): os.path.join('.', 'test_json', 'qry_int.json'),
+        ('qry', 'adv'): os.path.join('.', 'test_json', 'qry_adv.json'),
+    }
+
+    return json_files[test_type]
+
+
+def extract_json_objects(json_file):
+
+    result = False
+    global json_file_data
+
+    with open(json_file) as f:
+        for line in f:
+            try:
+                if line is None:
+                    break
+                json_file_data.append(json.loads(line))
+            except:
+                logger.exception("Error Reading JSON FIle: %s", json_file)
+                logger.debug("LINE:")
+                sys.exit()
+        result = True
+    return result
 
 def validate_message(message):
-    #Find corrosponding record in file
-    # return true/false
-    pass
-    json_msg = json.loads(message)
+    """
+    Validates JSON result against json file
+    :param message: ODE  Message
+    :return: Result of evaluation in Tuple Form
+    """
+
+    key = 'dateTime'
+    logger.debug("Message value: %s", message[key])
+
+    output = [record for record in json_file_data if record[key]== message[key]]
+    logger.debug("Search output: %s",output)
+    # for x in json_file_data:
+    #     logger.debug("JSON Valu: %s", x[key] )
+    #
+    #     if x[key] == message[key]:
+    #         output.append(x)
+    logger.debug(len(output))
+    if output:
+        logger.debug(output[0].keys())
+    result = None
+    information = None
 
 
+    return (result,information)
 
-#Command Line Parser Methods
 
+# Command Line Parser Methods
 def get_parser():
+    """
+    Creaates and returns Parser object
+    :return: optparse.OptionParser
+    """
     parser = OptionParser(
-        description ="Situation Data App Example ",
-        conflict_handler ="resolve")
+        description="Situation Data App Example",
+        conflict_handler="resolve")
 
-    parser.add_option('-?', action='callback', callback=print_help,help=SUPPRESS_HELP)
+    parser.add_option('-?', action='callback', callback=print_help, help=SUPPRESS_HELP)
 
-    req_group = OptionGroup(parser,"Required Parameters")
-    req_group.add_option('-t' ,'--type', help = 'Subscription Type Query - qry, Subscription = sub )',
+    req_group = OptionGroup(parser, "Required Parameters")
+    req_group.add_option('-t', '--type', help='Subscription Type Query - qry, Subscription = sub )',
                          metavar='type',
-                         dest = 'SUB_TYPE',
-                         default = 'sub')
+                         dest='SUB_TYPE',
+                         default='sub')
 
-    req_group.add_option('-d', '--data', help = 'Data Type (Vehicle, Intersection, Aggregate, Advisory)',
+    req_group.add_option('-d', '--data', help='Data Type (Vehicle, Intersection, Aggregate, Advisory)',
                          metavar='data',
-                         dest = 'DATA',
-                         default = 'veh')
+                         dest='DATA',
+                         default='veh')
 
-    req_group.add_option('-h','--host', help='Hostname',
-                         dest= 'HOST',
+    req_group.add_option('-h', '--host', help='Hostname',
+                         metavar='host',
+                         dest='HOST',
                          default='localhost:8080/ode')
 
     parser.add_option_group(req_group)
 
-    # group = OptionGroup(parser, "Optional Parameters")
-    #
-    # group.add_option('-','--archive_root', help='',
-    #                      metavar='',
-    #                      dest='',
-    #                      default = "", )
-    # parser.add_option_group(group)
+    group = OptionGroup(parser, "Optional Parameters")
+
+    group.add_option('-f', '--file', help='Full Path to a File containing JSON output that will be used to'
+                                          'validaate ODE output instead of default JSON files',
+                     metavar='file',
+                     dest='VALIDATION_FILE',
+                     default=None, )
+    group.add_option('-c', '--config', help='Full path to config file File that can be used to override all Settings.'
+                                            'Config file will override all command line parameters',
+                     metavar='config_file',
+                     dest='CONFIG_FILE',
+                     default=None)
+    parser.add_option_group(group)
 
     return parser
 
@@ -154,57 +280,91 @@ def usage(parser):
     sys.exit(2)
 
 
-def print_help(option, opt_str, value, parser,*args, **kwargs):
+def print_help(option, opt_str, value, parser, *args, **kwargs):
     """
     Call back function to handle printing help when passed -?
     Utitilized instead of updating validation method
     """
     usage(parser)
 
+
 def _main():
     parser = get_parser()
     (options, args) = parser.parse_args()
+
     config.update(vars(options))
+    if config.get('CONFIG_FILE'):
+        cp = parse_config_file(config['CONFIG_FILE'])
+
     _run_main(config)
 
+def parse_config_file(file_path):
+
+    config_file = ConfigParser.ConfigParser()
+    config_file.optionxform = str
+    try:
+        config_file.readfp(open(file_path))
+    except Exception as e:
+        logger.exception('Unable to Open Config File')
+        sys.exit(1)
+    if config_file.has_section('ode'):
+        config['HOST']=config_file.get('ode','host')
+        config['SUB_TYPE']=config_file.get('ode','subscriptionType')
+        config['DATA']=config_file.get('ode','dataType')
+        config['VALIDATION_FILE']=config_file.get('ode','validationFile')
+
+    if config_file.has_section('serviceRegion'):
+        if config.get('SUB_TYPE') and config.get('SUB_TYPE')== 'sub':
+            for key,value in config_file.items('serviceRegion'):
+                area[key]=value # Point is Tuple in form of (Name, Value)
+        else: # Update Query related Params
+            print config_file.items('serviceRegion')
+            for key,value in config_file.items('serviceRegion'):
+                print (key,value)
+                qry_subs[config['DATA']][key]=value
+
+    if config_file.has_section('queryParams'):
+        common_params['limit']=config_file.get('queryParams','limit')
+        common_params['skip']=config_file.get('queryParams','skip')
+
+
+    return config_file
 
 def _run_main(config):
-
     # host = "localhost:10494"
     # socket_url = "ws://localhost:10494/ode/api/ws/sub/ints" # veh, int,agg,
-#	ws://ec2-52-6-61-205.compute-1.amazonaws.com/ode/api/ws/qry/int  #adv, int, veh
+    #	ws://ec2-52-6-61-205.compute-1.amazonaws.com/ode/api/ws/qry/int  #adv, int, veh
 
-    subscription_type=config['SUB_TYPE']
-
+    subscription_type = config['SUB_TYPE']
+    global msg
 
     if subscription_type == 'sub':
-        global msg
         msg = area
         uri = 'sub/{0}'.format(config['DATA'])
-
     else:
         msg = qry_subs[config['DATA']]
-        msg.update(common_params) # add common params
+        msg.update(common_params)  # add common params
         uri = 'qry/{0}'.format(config['DATA'])
 
-    socket_url = "ws://{0}/api/ws/{1}".format(config['HOST'],uri)
-
+    socket_url = "ws://{0}/api/ws/{1}".format(config['HOST'], uri)
 
     global json_file
-    get_json_file()
-    json_file = "Some file"
-    print "Sending info"
-    print socket_url
-    print msg
-    print '==============='
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp(socket_url,#"ws://echo.websocket.example/",
-                                on_message = on_message,
-                                on_error = on_error,
-                                on_close = on_close,
-                               )
-    ws.on_open = on_open2
 
+    if config.get('VALIDATION_FILE'):
+        json_file = config.get('VALIDATION_FILE')
+    else:
+        json_file = get_json_file((subscription_type, config['DATA']))
+    extract_json_objects(json_file)
+
+    print "Web socket URL: {}".format(socket_url)
+    print '++++++++++'
+    websocket.enableTrace(False)
+    ws = websocket.WebSocketApp(socket_url,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close,
+                                )
+    ws.on_open = on_open2
 
     ws.run_forever()
 
@@ -212,12 +372,15 @@ if __name__ == "__main__":
     #_main() # Parse Command line options
 
     # Test harness code
-
     parser = get_parser()
     (options, args) = parser.parse_args()
     config.update(vars(options))
-    config["SUB_TYPE"]='qry'  # sub or qry
-    config["DATA"]='veh'
-    config['HOST'] = "ec2-52-6-61-205.compute-1.amazonaws.com:8080/ode"
-    common_params['limit']="100"
+    config['CONFIG_FILE']='.//sample_config.ini'
+    if config.get('CONFIG_FILE'):
+        cp = parse_config_file(config['CONFIG_FILE'])
+
+    # config["SUB_TYPE"] = 'qry'  # sub or qry
+    # config["DATA"] = 'veh'  # int, veh, adv, agg
+    # config['HOST'] = "ec2-52-6-61-205.compute-1.amazonaws.com:8080/ode"
+    # common_params['limit'] = "100"
     _run_main(config)
