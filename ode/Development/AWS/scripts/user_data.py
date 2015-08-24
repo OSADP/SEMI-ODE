8050d987fa2a03eaf7d6625a31527c7354769042
Matthew Kowalsky <kowalsky_matthew@bah.com>
2015-08-24 16:56:36 -0400

    ODE-49 and ODE-112. Resolved minor issues in cloud formation template by fixing the python script.

--- ode/Development/AWS/scripts/user_data.py
+++ ode/Development/AWS/scripts/user_data.py
@@ -14,12 +14,13 @@ def hadoop_work_node_userData(instance):
         '# Setup correct file ownership\n',
         'ambari_agent=/etc/ambari-agent/conf/ambari-agent.ini\n',
         "sed -i 's/hostname=.*/hostname=", GetAtt(instance, 'PrivateDnsName'), "/g' $ambari_agent\n",
-        "sleep 60 \n",
+        'sleep 60 \n',
+        'chkconfig ambari-agent on\n'
         'ambari-agent start\n',
         # Format and Mount Drive at
         'mkdir /data\n',
-        "mkfs -t ext4 /dev/xvdb\n",
-        "mount /dev/xvdb /data\n",
+        'mkfs -t ext4 /dev/xvdb\n',
+        'mount /dev/xvdb /data\n',
         "echo \"/dev/xvdb' /data ext4 defaults,nofail 0 2\" >> /etc/fstab\n",
         # "service firewalld stop\n"
     ]))
@@ -33,7 +34,8 @@ def install_ambari_server():
         'wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.1.0/ambari.repo -O /etc/yum.repos.d/ambari.repo\n'
         'yum install -y ambari-server\n'
         'ambari-server setup --silent\n',
-        'sleep 10\n'
+        'sleep 10\n',
+        'chkconfig ambari-server on\n',
         'ambari-server start\n',
         # Format and Mount Drive at
         'mkdir /data\n',
