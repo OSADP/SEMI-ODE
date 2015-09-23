984811e96a0148c01cc0bbe8e60eebc172b94d6e
whipp_alexander <whipp_alexander@bah.com>
2015-09-23 11:30:04 -0400

    Adding mac build local shell script, and added depedency for javax.websocket in pom.xml of webapp

---
+++ ode/Development/apps/mac-build-local.sh
@@ -0,0 +1,3 @@
+#!/bin/bash          
+mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=lib/osstoed.jar -DgroupId=asn.1 -DartifactId=oss.runtime -Dversion=6.1
+mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=lib/semi-asn.1.jar -DgroupId=asn.1 -DartifactId=semi -Dversion=2.2
