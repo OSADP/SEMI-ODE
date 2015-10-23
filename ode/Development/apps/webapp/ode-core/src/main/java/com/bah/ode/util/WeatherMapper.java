67c46762024741cd2cb37c7c14aca19e1cd2c9e5
Alexander Whipp <whipp_alexander@bah.com>
2015-10-23 18:21:47 -0400

    ODE-41
    Integrates the static weather data into vehicle data if flags are set.

---
+++ ode/Development/apps/webapp/ode-core/src/main/java/com/bah/ode/util/WeatherMapper.java
@@ -0,0 +1,32 @@
+package com.bah.ode.util;
+
+import java.io.Serializable;
+
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.Row;
+
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class WeatherMapper implements Function<Row, String>, Serializable {
+
+	private static final long serialVersionUID = 4620485681770070995L;
+	private String[] weatherHeader;
+
+	public WeatherMapper(String[] columns) {
+		weatherHeader = columns;
+	}
+
+	@Override
+	public String call(Row row) throws Exception {
+
+		ObjectNode node = JsonNodeFactory.instance.objectNode();
+		
+		for (int i = 0; i < row.length(); i ++ ){
+			node.put(weatherHeader[i], row.getString(i));
+		}
+		
+		return node.toString();
+	}
+
+}
--- ode/Development/apps/webapp/ode-spark/src/main/java/com/bah/ode/spark/WeatherIntegrator.java
+++ ode/Development/apps/webapp/ode-spark/src/main/java/com/bah/ode/spark/WeatherIntegrator.java
@@ -1,24 +1,57 @@
 package com.bah.ode.spark;
 
+import java.awt.geom.Point2D;
+import java.util.List;
+
 import org.apache.spark.api.java.function.PairFunction;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import com.bah.ode.util.GeoUtils;
+import com.bah.ode.util.JsonUtils;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import scala.Tuple2;
 
 public class WeatherIntegrator implements  PairFunction<Tuple2<String, Tuple2<String, String>>, String, Tuple2<String,String>> {
 
-	private static final long serialVersionUID = 898L;
-	private static Logger logger = LoggerFactory
-			.getLogger(WeatherIntegrator.class);
+	private static final long serialVersionUID = 2040465851629473055L;
+	private List<String> weatherData = null;
+	private String[] columns = null;
+
+	public WeatherIntegrator(List<String> weatherData, String[] columns) {
+		this.weatherData = weatherData;
+		this.columns = columns;
+	}
 
 	@Override
 	public Tuple2<String, Tuple2<String, String>> call(Tuple2<String, Tuple2<String, String>> record)
 			throws Exception {
-		logger.info("Topic: " + record._1());
-		logger.info("KEY: " + record._2()._1());
-		logger.info("VALUE: " + record._2()._2());
-        return record;
+
+		ObjectNode vehicledata = JsonUtils.toObjectNode(record._2()._1());
+		Point2D rddLoc = GeoUtils.latLngToMap(vehicledata.get("latitude").asDouble(),vehicledata.get("longitude").asDouble());
+
+		ObjectNode closestNode = null;
+		Double distance = Double.MAX_VALUE;
+
+		for(String s : weatherData){
+			ObjectNode temp = JsonUtils.toObjectNode(s);
+			if(closestNode == null){
+				closestNode = temp;
+				distance = rddLoc.distance(GeoUtils.latLngToMap(temp.get("Latitude").asDouble(), temp.get("Longitude").asDouble()));
+			}else{
+				Point2D tempLoc = GeoUtils.latLngToMap(temp.get("Latitude").asDouble(),temp.get("Longitude").asDouble());
+				double calcDist = rddLoc.distance(tempLoc);
+				if(calcDist < distance){
+					distance = calcDist; 
+					closestNode = temp;
+				}
+			}
+		}
+
+		for(String s : columns){
+			vehicledata.put(s, closestNode.get(s).asText());
+		}
+		
+		return new Tuple2<String, Tuple2<String, String>>(record._1(), new Tuple2<String,String>(vehicledata.toString(), record._2()._2()));
 
 	}
 
