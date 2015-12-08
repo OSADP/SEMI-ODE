0d7f0ed83dc5b1ae9a11027271f41c20a006ef2d
Alexander Whipp <whipp_alexander@bah.com>
2015-12-08 13:50:06 -0500

    ODE-38 ODE-41
    Adds sanitation logic, pre-testing
    Adds conditional to verify weather JSON objects are valid to not get
    null pointer exceptions

--- ode/Development/apps/webapp/ode-spark/src/main/java/com/bah/ode/spark/RecordSanitizer.java
+++ ode/Development/apps/webapp/ode-spark/src/main/java/com/bah/ode/spark/RecordSanitizer.java
@@ -15,7 +15,6 @@ import scala.Tuple2;
 public class RecordSanitizer implements  PairFunction<Tuple2<String, Tuple2<String, String>>, String, Tuple2<String,String>> {
 
 	private static final long serialVersionUID = 2040465851629473055L;
-	private static Logger logger = Logger.getLogger(RecordSanitizer.class);
 	private List<String> sanitizationData = null;
 
 	public RecordSanitizer(List<String> sanitizationData) {
@@ -23,18 +22,57 @@ public class RecordSanitizer implements  PairFunction<Tuple2<String, Tuple2<Stri
 	}
 
 	@Override
-	public Tuple2<String, Tuple2<String, String>> call(Tuple2<String, Tuple2<String, String>> record)
-	throws Exception {
+	public Tuple2<String, Tuple2<String, String>> call(Tuple2<String, Tuple2<String, String>> record) throws Exception {
 
 		ObjectNode vehicledata = JsonUtils.toObjectNode(record._2()._1());
 		double record_latitude = vehicledata.get("latitude").asDouble();
 		double record_longitude = vehicledata.get("longitude").asDouble();
-		double speed = vehicledata.get("speed").asDouble();
-		if(speed > 5){
-			return record;
-		}else{
-			return null;
+		for(String sanitizationJSON : sanitizationData){
+
+			/* 
+			 * boolean that determines if it meets an exception for being hidden, 
+			 * if true then continue looking for another box
+			 * Reason: if one box restricts it later on then it should not be shown.
+			 */
+			boolean excepted = false;
+
+			ObjectNode sanitizationBox = JsonUtils.toObjectNode(sanitizationJSON);
+			double nwLat = sanitizationBox.get("nwLat").asDouble();
+			double nwLon = sanitizationBox.get("nwLon").asDouble();
+			double seLat = sanitizationBox.get("seLat").asDouble();
+			double seLon = sanitizationBox.get("seLon").asDouble();
+
+			/* Inside a sanitizationBox*/
+			if(record_latitude >= nwLat && record_latitude <= seLat && record_longitude >= nwLon && record_longitude <= seLon){
+
+				/* Inside sanitizationBox, but outside of speed limits */
+				if(sanitizationBox.has("minSpeed") && sanitizationBox.has("maxSpeed") && vehicledata.has("speed")){
+					double minSpeed = sanitizationBox.get("minSpeed").asDouble();
+					double maxSpeed = sanitizationBox.get("maxSpeed").asDouble();
+					double record_speed = vehicledata.get("speed").asDouble();
+					if(record_speed < minSpeed || record_speed > maxSpeed){
+						excepted = true;
+					}
+				}
+
+				/* Inside a sanitizationBox, but meets groupID exception*/
+				if(sanitizationBox.has("exceptions") && vehicledata.has("groupId")){
+					String[] groupIDs = sanitizationBox.get("exceptions").asText().split(",");
+					String record_groupID = vehicledata.get("groupId").asText();
+					for(String excludedID : groupIDs){
+						if(excludedID.contains(record_groupID)){
+							excepted = true;
+						}
+					}
+				}
+
+				/* Should not be returned because it meets the bounding box record */
+				if(!excepted)
+					return null;
+			}
 		}
+
+		return record;
 	}
 
 }
--- ode/Development/apps/webapp/ode-spark/src/main/java/com/bah/ode/spark/WeatherIntegrator.java
+++ ode/Development/apps/webapp/ode-spark/src/main/java/com/bah/ode/spark/WeatherIntegrator.java
@@ -22,61 +22,67 @@ public class WeatherIntegrator implements  PairFunction<Tuple2<String, Tuple2<St
 
 	@Override
 	public Tuple2<String, Tuple2<String, String>> call(Tuple2<String, Tuple2<String, String>> record)
-	throws Exception {
+			throws Exception {
 
 		ObjectNode vehicledata = JsonUtils.toObjectNode(record._2()._1());
-		Point2D rddLoc = GeoUtils.latLngToMap(vehicledata.get("latitude").asDouble(),vehicledata.get("longitude").asDouble());
+		if(vehicledata.has("latitude") && vehicledata.has("longitude")){
+			Point2D rddLoc = GeoUtils.latLngToMap(vehicledata.get("latitude").asDouble(),vehicledata.get("longitude").asDouble());
 
-		ObjectNode closestAirPressure = null;
-		Double distanceAP = Double.MAX_VALUE;
+			ObjectNode closestAirPressure = null;
+			Double distanceAP = Double.MAX_VALUE;
 
-		ObjectNode closestAirTemperature = null;
-		Double distanceAT = Double.MAX_VALUE;
+			ObjectNode closestAirTemperature = null;
+			Double distanceAT = Double.MAX_VALUE;
 
-		for(String s : weatherData){
-			ObjectNode temp = JsonUtils.toObjectNode(s);
-			switch(temp.get("ObsTypeName").asText()){
+			for(String s : weatherData){
+				ObjectNode temp = JsonUtils.toObjectNode(s);
+				if(temp.has("Latitude") && temp.has("Longitude") && temp.has("ObsTypeName")){
+					switch(temp.get("ObsTypeName").asText()){
 
-				/* Atmospheric Air Pressure Source */
-				case "essAtmosphericPressure":
-				if(closestAirPressure == null){
-					closestAirPressure = temp;
-					distanceAP = rddLoc.distance(GeoUtils.latLngToMap(temp.get("Latitude").asDouble(), temp.get("Longitude").asDouble()));
-				}else{
-					Point2D tempLoc = GeoUtils.latLngToMap(temp.get("Latitude").asDouble(),temp.get("Longitude").asDouble());
-					double calcDist = rddLoc.distance(tempLoc);
-					if(calcDist < distanceAP){
-						distanceAP = calcDist;
-						closestAirPressure = temp;
+					/* Atmospheric Air Pressure Source */
+					case "essAtmosphericPressure":
+						if(closestAirPressure == null){
+							closestAirPressure = temp;
+							distanceAP = rddLoc.distance(GeoUtils.latLngToMap(temp.get("Latitude").asDouble(), temp.get("Longitude").asDouble()));
+						}else{
+							Point2D tempLoc = GeoUtils.latLngToMap(temp.get("Latitude").asDouble(),temp.get("Longitude").asDouble());
+							double calcDist = rddLoc.distance(tempLoc);
+							if(calcDist < distanceAP){
+								distanceAP = calcDist;
+								closestAirPressure = temp;
+							}
+						}
+						break;
+
+						/* Atmospheric Air Temperature Source */
+					case "essAirTemperature":
+						if(closestAirTemperature == null){
+							closestAirTemperature = temp;
+							distanceAT = rddLoc.distance(GeoUtils.latLngToMap(temp.get("Latitude").asDouble(), temp.get("Longitude").asDouble()));
+						}else{
+							Point2D tempLoc = GeoUtils.latLngToMap(temp.get("Latitude").asDouble(),temp.get("Longitude").asDouble());
+							double calcDist = rddLoc.distance(tempLoc);
+							if(calcDist < distanceAT){
+								distanceAT = calcDist;
+								closestAirTemperature = temp;
+							}
+						}
+						break;
+					default:
+						break;
 					}
 				}
-				break;
-
-				/* Atmospheric Air Temperature Source */
-				case "essAirTemperature":
-				if(closestAirTemperature == null){
-					closestAirTemperature = temp;
-					distanceAT = rddLoc.distance(GeoUtils.latLngToMap(temp.get("Latitude").asDouble(), temp.get("Longitude").asDouble()));
-				}else{
-					Point2D tempLoc = GeoUtils.latLngToMap(temp.get("Latitude").asDouble(),temp.get("Longitude").asDouble());
-					double calcDist = rddLoc.distance(tempLoc);
-					if(calcDist < distanceAT){
-						distanceAT = calcDist;
-						closestAirTemperature = temp;
-					}
-				}
-				break;
-				default:
-				break;
 			}
-		}
 
-		if(closestAirPressure != null && !vehicledata.has("weatherAirPres")){
-			vehicledata.put("weatherAirPres", closestAirPressure.get("Observation").asText());
-		}
-		
-		if(closestAirTemperature != null && !vehicledata.has("weatherAirTemp")){
-			vehicledata.put("weatherAirTemp", closestAirTemperature.get("Observation").asText());
+			if(closestAirPressure != null && !vehicledata.has("weatherAirPres")){
+				vehicledata.put("weatherAirPres", closestAirPressure.get("Observation").asText());
+			}
+
+			if(closestAirTemperature != null && !vehicledata.has("weatherAirTemp")){
+				vehicledata.put("weatherAirTemp", closestAirTemperature.get("Observation").asText());
+			}
+		}else{
+			return record;
 		}
 
 		return new Tuple2<String, Tuple2<String, String>>(record._1(), new Tuple2<String,String>(vehicledata.toString(), record._2()._2()));
