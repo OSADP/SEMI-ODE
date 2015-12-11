6d588a935247a8f7e6e57a982d18f17b6ac2de86
Matthew Kowalsky <kowalsky_matthew@bah.com>
2015-12-11 14:12:53 -0500

    updated test configs

--- ode/Development/emulatedClientApps/SituationalDataApp/tests/smokeTests.py
+++ ode/Development/emulatedClientApps/SituationalDataApp/tests/smokeTests.py
@@ -5,16 +5,14 @@ import sys
 from test_ODE_Query_API import ODE_Query_Downloads_Tests
 from test_ODE_Subscription_API import ODE_Subscription_Connectivity_Tests
 
-#from test_ODE_Multiple_Clients import ODE_Basic_Subscription_Multiple_Clients_Tests
+from test_ODE_Multiple_Clients import ODE_Basic_Subscription_Multiple_Clients_Tests
 
-
-
-if __name__ =='__main__':
+if __name__ == '__main__':
     suite = unittest.TestSuite()
 
     suite.addTests(unittest.makeSuite(ODE_Subscription_Connectivity_Tests))
     suite.addTests(unittest.makeSuite(ODE_Query_Downloads_Tests))
-    #suite.addTest(unittest.makeSuite(ODE_Basic_Subscription_Multiple_Clients_Tests))
+    suite.addTest(unittest.makeSuite(ODE_Basic_Subscription_Multiple_Clients_Tests))
 
     runner = unittest.TextTestRunner(stream=sys.stdout)
     runner.run(suite)
