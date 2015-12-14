fe0e007c3dc41fb8d706e7413ac576298fe6b5b2
Hamid Musavi <musavi_hamid@bah.com>
2015-12-14 12:59:33 -0500

    ODE-172: Upgrade OSS Nokalva ASN.1 compiler to new version 6.2 Updated ODE with upgraded ASN.1 compiler.

--- ode/Development/apps/mac-build-local.sh
+++ ode/Development/apps/mac-build-local.sh
@@ -1,3 +1,3 @@
 #!/bin/bash          
-mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=lib/osstoed.jar -DgroupId=asn.1 -DartifactId=oss.runtime -Dversion=6.1
-mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=lib/semi-asn.1.jar -DgroupId=asn.1 -DartifactId=semi -Dversion=2.2
+mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=../ASN.1/JavaAPI/semi/target/osstoed.jar -DgroupId=asn.1 -DartifactId=oss.runtime -Dversion=6.1
+mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=../ASN.1/JavaAPI/semi/target/semi-asn.1.jar -DgroupId=asn.1 -DartifactId=semi -Dversion=2.2
