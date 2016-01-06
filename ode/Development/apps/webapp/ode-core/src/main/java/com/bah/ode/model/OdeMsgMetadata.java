e3d70333fa32b75c92459a8173534d32f8757d25
whipp_alexander <whipp_alexander@bah.com>
2016-01-06 11:04:45 -0500

    ODE-68 OdeMsgMetatadata updates

--- ode/Development/apps/webapp/ode-core/src/main/java/com/bah/ode/model/OdeMsgMetadata.java
+++ ode/Development/apps/webapp/ode-core/src/main/java/com/bah/ode/model/OdeMsgMetadata.java
@@ -1,55 +1,93 @@
 package com.bah.ode.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
 public class OdeMsgMetadata extends OdeMessage {
 
-   private static final long serialVersionUID = 3979762143291085955L;
-   
-   private String payloadType;
+	private static final long serialVersionUID = 3979762143291085955L;
 
-   public OdeMsgMetadata(OdeMsgPayload payload) {
-      this.payloadType = OdeDataType.getByClazz(payload.getClass()).getShortName();
-   }
+	private String payloadType;
+	private List<OdeViolation> violations;
 
-   public String getPayloadType() {
-      return payloadType;
-   }
+	public OdeMsgMetadata(OdeMsgPayload payload) {
+		this.payloadType = OdeDataType.getByClazz(payload.getClass()).getShortName();
+		if(this.payloadType.equals("veh")) // to only display violations on vehicle records
+			this.violations = new ArrayList<OdeViolation>();
+	}
 
-   public OdeMsgMetadata setPayloadType(OdeDataType payloadType) {
-      this.payloadType = payloadType.getShortName();
-      return this;
-   }
-   
-   public OdeMsgMetadata setPayloadType(String payloadType) {
-      this.payloadType = payloadType;
-      return this;
-   }
+	public OdeMsgMetadata(OdeMsgPayload payload, JsonNode violations) {
+		this.payloadType = OdeDataType.getByClazz(payload.getClass()).getShortName();
 
-   @Override
-   public int hashCode() {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result
-            + ((payloadType == null) ? 0 : payloadType.hashCode());
-      return result;
-   }
+		if(this.payloadType.equals("veh")){ // to only display violations on vehicle records
+			this.violations = new ArrayList<OdeViolation>();
+			if (violations.isArray()) {
+				for (final JsonNode objNode : violations) {
+					if(objNode.get("fieldName") != null && objNode.get("validMin") != null && objNode.get("validMax") != null)
+						this.violations.add(new OdeViolation(objNode.get("fieldName").asText(), objNode.get("validMin").asDouble(), objNode.get("validMax").asDouble()));
+				}
+			}
+		}
+	}
 
-   @Override
-   public boolean equals(Object obj) {
-      if (this == obj)
-         return true;
-      if (obj == null)
-         return false;
-      if (getClass() != obj.getClass())
-         return false;
-      OdeMsgMetadata other = (OdeMsgMetadata) obj;
-      if (payloadType == null) {
-         if (other.payloadType != null)
-            return false;
-      } else if (!payloadType.equals(other.payloadType))
-         return false;
-      return true;
-   }
-   
-   
+	public String getPayloadType() {
+		return payloadType;
+	}
+
+	public OdeMsgMetadata setPayloadType(OdeDataType payloadType) {
+		this.payloadType = payloadType.getShortName();
+		return this;
+	}
+
+	public OdeMsgMetadata setPayloadType(String payloadType) {
+		this.payloadType = payloadType;
+		return this;
+	}
+
+	public List<OdeViolation> getViolations() {
+		return violations;
+	}
+
+	public OdeMsgMetadata setViolations(List<OdeViolation> violations) {
+		this.violations = violations;
+		return this;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((payloadType == null) ? 0 : payloadType.hashCode());
+		result = prime * result
+				+ ((violations == null) ? 0 : violations.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OdeMsgMetadata other = (OdeMsgMetadata) obj;
+		if (payloadType == null) {
+			if (other.payloadType != null)
+				return false;
+		} else if (!payloadType.equals(other.payloadType))
+			return false;
+		if (violations == null) {
+			if (other.violations != null)
+				return false;
+		} else if (!violations.equals(other.violations))
+			return false;
+		return true;
+	}
+
+
 
 }
