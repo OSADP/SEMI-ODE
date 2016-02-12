c28a8f320d5656bb18fa4ad871de9b24d4b14826
Matthew Kowalsky <kowalsky_matthew@bah.com>
2016-02-12 12:38:07 -0500

    scripts used to install grafana / graphite and collectd on server. may need to be adjusted when using it the future as some values ( like hostnames ) can change in the future.

--- ode/Vagrant/ode-elk/grafan.sh
+++ ode/Vagrant/ode-elk/grafan.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
-sudo apt-get install -y graphite-carbon
-echo "CARBON_CACHE_ENABLED=true" > /etc/default/graphite-carbon
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -q -y --force-yes install graphite-carbon
+echo "CARBON_CACHE_ENABLED=true" | sudo tee /etc/default/graphite-carbon
 
-service carbon-cache start
+sudo service carbon-cache start
 
-apt-get install -y graphite-web apache2 apache2-mpm-worker libapache2-mod-wsgi
-
+sudo apt-get install -y graphite-web apache2 apache2-mpm-worker libapache2-mod-wsgi
 # Prepare and migrate the SQLite database
 sudo chown _graphite /var/lib/graphite
-sudo -u _graphite graphite-manage syncdb --noinput
-
+sudo -u _graphite graphite-manage syncdb --noinput	
 # Configure httpd
 sudo rm -f /etc/apache2/sites-enabled/000-default.conf
 sudo cp /usr/share/graphite-web/apache2-graphite.conf /etc/apache2/sites-enabled/graphite.conf
@@ -18,11 +17,15 @@ sudo service apache2 restart
 #sudo rm -f /etc/apache2/sites-enabled/000-default.conf
 #sudo cp /usr/share/graphite-web/apache2-graphite.conf /etc/apache2/sites-enabled/graphite.conf
 #sudo service apache2 restart
+## Install on and start grafana on Port 3000
 
 sudo wget https://grafanarel.s3.amazonaws.com/builds/grafana_2.0.2_amd64.deb
 sudo apt-get install -y adduser libfontconfig
 sudo dpkg -i grafana_2.0.2_amd64.deb
-
-sudo echo "alias /grafana /usr/share/grafana" > /etc/apache2/sites-enabled/grafana.conf
+sudo service grafana-server start
 
 sudo service apache2 restart
+#echo "alias /grafana /usr/share/grafana" | sudo tee /etc/apache2/sites-enabled/grafana.conf
+# sudo apt-get install collectd 
+
+
