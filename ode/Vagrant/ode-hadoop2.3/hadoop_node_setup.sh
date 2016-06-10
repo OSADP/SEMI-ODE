9fa8b9410b1fa380361be3c1662a688f56309c1f
Hamid Musavi <musavi_hamid@bah.com>
2016-06-10 17:27:00 -0400

    General clean up.

--- ode/Vagrant/ode-hadoop2.3/hadoop_node_setup.sh
+++ ode/Vagrant/ode-hadoop2.3/hadoop_node_setup.sh
@@ -6,6 +6,8 @@ set -x      # Print commands and arguments
 echo ""
 echo "Done with Spark Install"
 echo "Disabling firewall"
+#sudo service iptables stop
+#sudo chkconfig iptables off
 sudo service firewalld stop
 sudo chkconfig firewalld off
 setenforce 0 
@@ -20,4 +22,12 @@ echo "Update Open SSL Library"
 sudo yum upgrade -y openssl  
 
 sudo cat /ode-common/public_key.txt >> /home/vagrant/.ssh/authorized_keys
+#echo "Install ambari Repo"
+#cd /etc/yum.repos.d && sudo curl -O http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.7.0/ambari.repo
+#echo "install Ambari Package"
+#sudo yum install -y ambari-server 
+#echo "Running Ambari Setup Script"
+#sudo ambari-server setup --silent
+#sudo ambari-server start
+#echo "Ambari Server Setup Complete"
 
