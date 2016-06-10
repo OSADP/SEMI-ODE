d742665fa9a00664306434bc3327f16a8ab5b57b
Hamid Musavi <musavi_hamid@bah.com>
2016-06-10 15:34:00 -0400

    Upgraded OSS Nokalva ASN.1 Compiler and Runtime to version 6.3.0 and license file to perpetual R&D Only license.

--- ode/Development/apps/build-local-repo.bat
+++ ode/Development/apps/build-local-repo.bat
@@ -1,3 +1,3 @@
 call mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=..\ASN.1\JavaAPI\semi\lib\osstoed.jar -DgroupId=asn.1 -DartifactId=oss.runtime -Dversion=6.3
-call mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=..\ASN.1\JavaAPI\semi\target\semi-2.2.jar -DgroupId=asn.1 -DartifactId=semi -Dversion=2.2
+call mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=..\ASN.1\JavaAPI\semi\target\semi-2.2.1.jar -DgroupId=asn.1 -DartifactId=semi -Dversion=2.2.1
 pause
