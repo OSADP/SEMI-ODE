b586d6c0889fbcd70ed0a44061bf3b7dfb50d7ba
Hamid Musavi <musavi_hamid@bah.com>
2016-06-10 15:34:00 -0400

    Upgraded OSS Nokalva ASN.1 Compiler and Runtime to version 6.3.0 and license file to perpetual R&D Only license.

--- ode/Development/ASN.1/JavaAPI/semi/build-semi-api.bat
+++ ode/Development/ASN.1/JavaAPI/semi/build-semi-api.bat
@@ -8,5 +8,5 @@ call "C:\Program Files (x86)\OSS Nokalva\asn1pjav\win32\6.3.0\bin\asn1pjav.exe" 
 
 REM build ASN.1 Java API Jar file and install it in the local Maven repository
 call C:\apache\maven\3.2.5\bin\mvn clean package
-call C:\apache\maven\3.2.5\bin\mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=target\semi-2.2.jar -DgroupId=asn.1 -DartifactId=semi -Dversion=2.2
+call C:\apache\maven\3.2.5\bin\mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=target\semi-2.2.1.jar -DgroupId=asn.1 -DartifactId=semi -Dversion=2.2.1
 pause
--- ode/Development/apps/build-remote-repo.bat
+++ ode/Development/apps/build-remote-repo.bat
@@ -1,3 +1,16 @@
+REM =======================================
+REM You must add the Nexus server ID and credentials 
+REM to your maven global settings.xml file OR your
+REM maven user settings.xml file located in you user
+REM home directory: C:\Users\yourusername\.m2\settings.xml
+REM
+REM    <server>
+REM      <id>cnsi-ode-repo</id>
+REM      <username>your.nexus.username</username>
+REM      <password>your.nexus.password</password>
+REM    </server>
+REM ========================================
+
 call C:\apache\maven\3.2.5\bin\mvn deploy:deploy-file -Durl=http://dropbox.cnsi.com/nexus/content/repositories/ODE/ -DrepositoryId=cnsi-ode-repo -DcreateChecksum=true -Dpackaging=jar -Dfile=..\ASN.1\JavaAPI\semi\lib\osstoed.jar -DgroupId=asn.1 -DartifactId=oss.runtime -Dversion=6.3
-call C:\apache\maven\3.2.5\bin\mvn deploy:deploy-file -Durl=http://dropbox.cnsi.com/nexus/content/repositories/ODE/ -DrepositoryId=cnsi-ode-repo -DcreateChecksum=true -Dpackaging=jar -Dfile=..\ASN.1\JavaAPI\semi\target\semi-2.2.jar -DgroupId=asn.1 -DartifactId=semi -Dversion=2.2
+call C:\apache\maven\3.2.5\bin\mvn deploy:deploy-file -Durl=http://dropbox.cnsi.com/nexus/content/repositories/ODE/ -DrepositoryId=cnsi-ode-repo -DcreateChecksum=true -Dpackaging=jar -Dfile=..\ASN.1\JavaAPI\semi\target\semi-2.2.1.jar -DgroupId=asn.1 -DartifactId=semi -Dversion=2.2.1
 pause
